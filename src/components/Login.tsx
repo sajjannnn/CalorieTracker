8d85d6c65035064302443bd1c8eb703e9f689cbd
Sajjan Kumar <sajjanaasht@gmail.com>
2026-01-07 20:01:19 +0530

    style: enhance Login component UI with improved layout and styling for form elements

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -76,25 +76,25 @@ const Login = () => {
   // } else {
   // }
   return (
-    <div className="h-screen md:grid grid-cols-12">
+    <div className="h-screen md:grid grid-cols-12 bg-[#f8f7f3]">
       <div className=" col-span-7">
         <img className="hidden md:block -z-5 h-screen object-cover object-center" src={LOGIN_BG_URL} alt="" />
 
       </div>
       <div className="h-full w-full flex  justify-center items-center col-span-5">
-        <form className=" w-[550px] h-[600px]  p-8 flex flex-col justify-center items-center md:border md:shadow-2xl">
-          <h1 className="w-3/5 font-bold text-2xl mb-10 md:text-5xl"> {isSignin ? "Sign In" : "Sign Up"} </h1>
+        <form className=" w-[550px] h-[600px] bg-white p-8 flex flex-col justify-center items-center md:border border-gray-300 rounded-2xl md:shadow-2xl">
+          <h1 className="w-3/5 font-bold text-2xl mb-10 md:text-4xl"> {isSignin ? "Sign In" : "Sign Up"} </h1>
           <div className="w-3/5 flex flex-col gap-4 md:gap-7">
-            {!isSignin && <input className="p-3 px-4  border" ref={name} type="text" placeholder="Name" />}
-            <input className="p-3 px-4 border border-black" ref={email} type="text" placeholder="Email Address" />
-            <input className="p-3 px-4 border border-black" ref={password} type="password" placeholder="Password" />
-            <p>{message}</p>
+            {!isSignin && <input className="p-3 px-4 bg-[#f8f7f3] border-gray-300 border rounded-2xl" ref={name} type="text" placeholder="Name" />}
+            <input className="p-3 px-4 border bg-[#f8f7f3] border-gray-300 rounded-2xl" ref={email} type="text" placeholder="Email Address" />
+            <input className="p-3 px-4 border bg-[#f8f7f3] border-gray-300 rounded-2xl" ref={password} type="password" placeholder="Password" />
+            <p className="text-gray-500 ">{message}</p>
           </div>
 
-          <button onClick={(e) => checkValidator(e)} className="bg-black text-white rounded px-8 py-2 w-3/5 text-3xl font-bold hover:bg-gray-400">
+          <button onClick={(e) => checkValidator(e)} className="bg-green-500  rounded-2xl hover:bg-green-400 text-white rounded px-8 py-2 w-3/5 text-xl hover:bg-gray-400">
             {isSignin ? "Sign In" : "Sign Up"}
           </button>
-          <p className="cursor-pointer m-5" onClick={LoginType}>
+          <p className="cursor-pointer m-5 text-gray-700" onClick={LoginType}>
             {" "}
             {isSignin ? "New? Sign Up Now" : "Already have an account? Sign In"}
           </p>
